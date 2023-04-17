6fce135075ca6b85da124956fd9c2fcc27d533f4
hongqi guo <hongqi.guo@wsu.edu>
2023-04-17 19:49:09 -0400

    Add files via upload
    let's main page and login,card page Mobile UI Responsive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,16 +7,11 @@ import { GlobaltaskService } from '../globaltask.service';
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-
 export class MainComponent {
   imagePath = 'assets/images';
   searchResults!: any[];
   tasks!: any[];
   searchKw!: string;
-  
-  currentPage: number = 1;
-  totalPages: number = 10;
-  isLoading: boolean = false;
 
   constructor(private globaltaskService : GlobaltaskService){
 
@@ -45,46 +40,4 @@ export class MainComponent {
     });
   }
 
-  async loadTasks() {
-    try {
-      this.isLoading = true;
-      const response = await this.globaltaskService.getTasksSlow(1, 10);
-  
-      if (response) {
-        this.totalPages = response.totalPages;
-        this.tasks = response.tasks;
-      }
-    } catch (error) {
-      console.error('Error loading initial tasks:', error);
-    } finally {
-      this.isLoading = false;
-    }
-  }
-  
-
-  async loadMoreTasks() {
-    if (this.currentPage >= this.totalPages) {
-      return;
-    }
-  
-    if (this.isLoading) {
-      return;
-    }
-  
-    this.isLoading = true;
-    this.currentPage++;
-  
-    try {
-      const response = await this.globaltaskService.getTasksSlow(this.currentPage, 10);
-  
-      if (response) {
-        this.totalPages = response.totalPages;
-        this.tasks = [...this.tasks, ...response.tasks];
-      }
-    } catch (error) {
-      console.error('Error loading more tasks:', error);
-    } finally {
-      this.isLoading = false;
-    }
-  }
 }
